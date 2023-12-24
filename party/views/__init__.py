f149c64e14f56b0b31cbbf171427b9033d462b5a
Forrest Bajbek <forrestbajbek@gmail.com>
2023-12-24 12:40:57 -0500

    Implemented gift feature

--- party/views/__init__.py
+++ party/views/__init__.py
@@ -1,4 +1,17 @@
+from .gift_registry_views import GiftRegistryPage, GiftUpdateFormPartial, GiftDetailPartial
+from .new_party_views import page_new_party, partial_check_party_date, partial_check_invitation
 from .party_list_views import PartyListPage
 from .party_details_views import PartyDetailPage, PartyDetailPartial
 
-__all__ = ["PartyListPage", "PartyDetailPage", "PartyDetailPartial"]
+
+__all__ = [
+    "PartyListPage",
+    "PartyDetailPage",
+    "PartyDetailPartial",
+    "page_new_parties",
+    "partial_check_party_date",
+    "partial_check_invitation",
+    "GiftRegistryPage",
+    "GiftUpdateFormPartial",
+    "GiftDetailPartial",
+]
